0b3239da38266ea9cf8ba0a6cf740886245d9cd0
alexandre jamarco <alexandre.j@gmail.com>
2020-04-25 12:44:50 +0100

    added theme

--- src/App.js
+++ src/App.js
@@ -1,34 +1,71 @@
 import React,{useState, useEffect} from 'react';
 import './App.css';
-import Login from './Components/Login/Login'
+// import Login from './Components/Login/Login'
 import Requests from './Libraries/Requests'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Navbar from "./Components/Navbar"
 
+//MUI
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+
+import themeObj from './Helpers/Theme'
+//pages
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import SignUp from './Pages/SignUp'
+
+const theme = createMuiTheme(themeObj);
 
 function App() {
-  const [email,setEmail] = useState(null);
-  console.log('email is ', email);
-  const handleSignin = (data) => {
-    localStorage.token = data.token;
-    setEmail(data.email);
-  }
-
-  useEffect(() => {
-    //if we have a token stored, we will try make a get request to /validate
-    if (localStorage.token){
-      Requests.validate(localStorage.token)
-        .then(data => {
-          setEmail(data.email);
-          localStorage.token = data.token;
-        })
-    }else console.log('noow');
-  }, [localStorage.token])
-
-  return (
-    <div className="App">
-      <h2>{email !== null ? `welcome ${email}` : "welcome stranger"}</h2>
-      <Login handleSignin={handleSignin}/>
-    </div>
-  );
+  return(
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar/>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/signup" component={SignUp}/>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
+  )
 }
 
 export default App;
+
+// this.setState({
+//   username: null
+// })
+// localStorage.removeItem("token")
+
+// const [email,setEmail] = useState(null);
+// console.log('email is ', email);
+// const handleSignin = (data) => {
+//   localStorage.token = data.token;
+//   setEmail(data.email);
+// }
+
+// useEffect(() => {
+//   //if we have a token stored, we will try make a get request to /validate
+//   if (localStorage.token){
+//     Requests.validate(localStorage.token)
+//       .then(data => {
+//         setEmail(data.email);
+//         localStorage.token = data.token;
+//       })
+//   }else console.log('noow');
+// }, [localStorage.token])
+
+// return (
+//   <div className="App">
+//     <h2>{email !== null ? `welcome ${email}` : "welcome stranger"}</h2>
+//     <Login handleSignin={handleSignin}/>
+//   </div>
+// );
+// }
