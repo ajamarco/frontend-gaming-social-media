d1294e1395981e81b3121e876c400e5a58538c87
alexandre jamarco <alexandre.j@gmail.com>
2020-05-08 20:35:57 +0100

    Updates

---
+++ precache-manifest.c194e82e7e481cf78f914f168df1384a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa971aeae1491f4135711a4f5c9a61c6",
+    "url": "/frontend-gaming-social-media/index.html"
+  },
+  {
+    "revision": "43876aeb22382bb20c11",
+    "url": "/frontend-gaming-social-media/static/css/main.ae19b17c.chunk.css"
+  },
+  {
+    "revision": "0fd3447d1e9638ad3228",
+    "url": "/frontend-gaming-social-media/static/js/2.08b97592.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/frontend-gaming-social-media/static/js/2.08b97592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43876aeb22382bb20c11",
+    "url": "/frontend-gaming-social-media/static/js/main.18b0e856.chunk.js"
+  },
+  {
+    "revision": "056eb59fb27ac85894ac",
+    "url": "/frontend-gaming-social-media/static/js/runtime-main.16740d31.js"
+  }
+]);
